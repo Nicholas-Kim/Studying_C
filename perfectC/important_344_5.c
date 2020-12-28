4d4fdec9d960f19bff111781a2d45a15cd97a99c
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2020-12-28 23:19:14 +0900

    Update important_344_5.c

--- perfectC/important_344_5.c
+++ perfectC/important_344_5.c
@@ -2,19 +2,36 @@
 
 int main()
 {
-	int j = 0;
-	for (int i = 1; i <= 100; i++)
-	{
-		for (j = 2; j < i; j++)
-		{
-			if (i % j == 0)
-				break;
-		}
-		if (i == j)
-		{
-			printf("%3d", i);
-			printf("\n");
-		}
-	}
-	return 0;
+    /*int j = 0;
+     for (int i = 1; i <= 100; i++)
+     {
+     for (j = 2; j < i; j++)
+     {
+     if (i % j == 0)
+     break;
+     }
+     if (i == j)
+     {
+     printf("%3d", i);
+     printf("\n");
+     }
+     }*/
+    
+    
+    
+    int flag =1;
+    for(int i=2;i<=100;i++){
+        flag = 1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                flag = 0;
+                break;
+            }
+        }
+        if(flag==1){
+            printf("%3d\n", i);
+        }
+        
+    }
+    return 0;
 }
